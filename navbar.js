0c68f28d169c7cf7a30a796491756d107a3eb488
RizaHKhan <khanriza@gmail.com>
2020-04-25 13:00:57 -0400

    Added Burger, javascript file

---
+++ navbar.js
@@ -0,0 +1,6 @@
+const toggler = document.querySelector(".toggler");
+const nav = document.querySelector(".dpd-nav");
+
+toggler.addEventListener("click", function () {
+  nav.classList.toggle("show");
+});
